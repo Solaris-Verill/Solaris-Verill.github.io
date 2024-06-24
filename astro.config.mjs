bd30eaad5245da31452c8d33ba14c5518fe002b1
Jonathan-Lanham <Solaris.verill@gmail.com>
2024-06-24 15:42:41 -0400

    Testing x3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://Solaris-Verill.github.io/',
+	site: 'https://Solaris-Verill.github.io',
 	integrations: [mdx(), sitemap()],
 });
